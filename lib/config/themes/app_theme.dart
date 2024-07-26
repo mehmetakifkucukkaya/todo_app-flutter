b65a6324cee2735810cf4b6f9b17985996385593
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-07-26 19:41:39 +0300

    Added app theme and strings

---
+++ lib/config/themes/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color buttonColor = Color(0xFF233098);
+  static const Color orange = Color(0xFFFCA01F);
+  static const Color green = Color(0xFF54AA76);
+  static const Color blueText = Color(0xFF223097);
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,28 @@
+// Uygulamada kullanılacak olan metinleri içeren sınıf
+
+class AppStrings {
+  // Genel Metinler
+  static const String appName = "ToDo App";
+  static const String todo = "ToDo";
+
+  // Görevler ile ilgili metinler
+  static const String addTodo = "Todo Ekle";
+  static const String deleteTodo = "Todo Sil";
+  static const String updateTodo = "Todo Güncelle";
+  static const String completeTodo = "Todo'yu Tamamla";
+
+  // Login İle ilgili metinler
+  static const String login = "Giriş Yap";
+  static const String register = "Kayıt Ol";
+  static const String logout = "Çıkış Yap";
+
+  // Input Alanları İle İlgili Metinler
+  static const String email = "E-posta";
+  static const String password = "Şifre";
+  static const String name = "İsim";
+  static const String lastName = "Soy İsim";
+
+  // Tablar İle İlgili Metinler
+  static const String completedTodos = "Tamamlanmış";
+  static const String notCompletedTodos = "Tamamlanmamış";
+}
